efaca3ae14c5061c9944dce7eee8f119939ae458
AnimalXing <66161458+AnimalXing@users.noreply.github.com>
2022-11-05 18:37:57 -0400

    question 2

--- start.r
+++ start.r
@@ -1,2 +1,15 @@
 databank<-read.table("/Users/davidsmac/Desktop/math/databank.txt", sep=",",
          header=T, na.strings=c("", "NA"))
+
+
+hic<-crash$hic
+sum(hic,na.rm=TRUE)
+
+group=cut(hic,breaks=c(0,400,800,1200,1600,4500),right=FALSE)
+count<-table(group)
+
+n<-length(hic)
+percent<-count/n*100
+percent
+cumpct<-cumsum(percent)
+cumpct
